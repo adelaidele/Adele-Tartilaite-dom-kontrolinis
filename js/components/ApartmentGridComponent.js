6fa3b68cdcacde6f730825289bb64c390d4b1933
adelaidele <tartilaitee@gmail.com>
2021-11-15 19:00:32 +0200

    5: komponentu pasiruosimas/testas

---
+++ js/components/ApartmentGridComponent.js
@@ -0,0 +1,10 @@
+class ApartmentsGridComponent {
+    constructor(){
+        this.init();
+    }
+
+    init = () => {;
+        this.htmlElement = document.createElement("div");
+        this.htmlElement.innerHTML = 'bandau laime';
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,5 @@
+const rootElement = document.querySelector("#root");
+
+const apartmentsGridComponent = new ApartmentsGridComponent();
+
+rootElement.appendChild(apartmentsGridComponent.htmlElement);
