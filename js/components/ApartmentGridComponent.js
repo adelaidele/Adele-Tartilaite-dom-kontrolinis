6de1209cc8e7ea277034a13ffb35b7ad21e72c97
adelaidele <tartilaitee@gmail.com>
2021-11-15 19:20:41 +0200

    6: component fetch apartments

--- js/components/ApartmentGridComponent.js
+++ js/components/ApartmentGridComponent.js
@@ -1,10 +1,36 @@
 class ApartmentsGridComponent {
     constructor(){
+        this.state = {
+            loading: false,
+            apartments: []
+          }
         this.init();
     }
 
+    fetchApartments = () => API.fetchApartments(this.saveApartments, alert);
+
+    saveApartments = (apartments) => {
+        this.state.apartments = apartments;
+        this.state.loading = false;
+
+        this.render();
+    }
+
     init = () => {;
+        this.state.loading = true;
+        this.fetchApartments();
         this.htmlElement = document.createElement("div");
         this.htmlElement.innerHTML = 'bandau laime';
+
+        this.render();
+    }
+
+    render = () => {
+        const { loading, apartments } = this.state;
+        if (loading) {
+          this.htmlElement.innerHTML = 'siunčiama...';
+        } else {
+          this.htmlElement.innerHTML = 'parsiųsta!';
+        }  
     }
 }
